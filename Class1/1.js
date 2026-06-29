a4b025bbd43d15ed1e07fcdf5474ac18c1a38b5e
Pragyan Nanda <pragyannanda567@gmail.com>
2026-06-29 14:29:17 +0530

    Backend Class1

---
+++ Class1/1.js
@@ -0,0 +1,7 @@
+console.log('welcome to backend')
+
+function sum(x,y){
+    console.log(x+y)
+}
+
+sum(10,20);
---
+++ Class1/2.js
@@ -0,0 +1,7 @@
+console.log('First');
+
+setTimeout(()=>{
+    console.log('Second')
+},3000);
+
+console.log('Third');
